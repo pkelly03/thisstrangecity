88f32651f103ee8d946144869fd3ee768ab80062
Paul Kelly <pkelly@equalexperts.com>
2013-02-18 10:35:37 +0000

    updated migration guide, no need to install local modules again

--- web-ui/migration.sh
+++ web-ui/migration.sh
@@ -1,8 +1,6 @@
 npm cache clean
 sudo npm install -g yo
-npm install generator-webapp 
-npm install generator-angular
-npm install generator-mocha
-npm install generator-testacular
 npm install && bower install
+
+# Test installation by running the following:
 grunt server
