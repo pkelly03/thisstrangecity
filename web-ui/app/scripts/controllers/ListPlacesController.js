9a3a288892dc10546602cfd70adabed2ca6edd1c
prasanth <vjprasanth@gmail.com>
2013-03-21 09:13:05 +0000

    ListPlaceController now look for the location of the user. Latitude and longitude is printed on the main screen.
    [PVJ]

--- web-ui/app/scripts/controllers/ListPlacesController.js
+++ web-ui/app/scripts/controllers/ListPlacesController.js
@@ -1,5 +1,11 @@
 'use strict';
 
-app.controller('ListPlacesController', function($scope, PlacesService) {
+app.controller('ListPlacesController', function ($scope, $window, PlacesService) {
     $scope.places = PlacesService.query();
+    $window.navigator.geolocation.getCurrentPosition(function (position) {
+        var latitude = position.coords.latitude;
+        var longitude = position.coords.longitude;
+        $scope.currentLocation = { lon:longitude, lat:latitude};
+    });
+
 });
--- web-ui/test/spec/controllers/ListPlacesControllerTest.js
+++ web-ui/test/spec/controllers/ListPlacesControllerTest.js
@@ -14,19 +14,27 @@ angular.module('appWithDummyService', ['ngResource'])
 
 describe('Controller: ListPlacesController', function () {
 
-    var $scope, ListPlacesController;
+    var $scope, ListPlacesController, $window, mockNavigator;
 
     beforeEach(module('webUiApp'));
     beforeEach(module('appWithDummyService'));
 
-    beforeEach(inject(function($rootScope, $controller, PlacesService) {
+    beforeEach(inject(function( $rootScope, $controller, PlacesService) {
         $scope = $rootScope.$new();
+        var geoCurrentPosition = function () {};
+        mockNavigator = {geolocation : { getCurrentPosition : geoCurrentPosition}};
+        $window = {navigator : mockNavigator};
+        spyOn($window.navigator.geolocation, 'getCurrentPosition');
         ListPlacesController = $controller('ListPlacesController', {
-          $scope: $scope 
+          $scope: $scope,
+            $window : $window
         });
     }));
 
     it('should make a call to PlacesService and copy value to coolPlacesList', inject(function (PlacesService) {
         expect($scope.places.length).toEqual(2);
+    }));
+    it('should make a call to navigator service', inject(function (PlacesService) {
+        expect($window.navigator.geolocation.getCurrentPosition).toHaveBeenCalled();
     }));
 });
